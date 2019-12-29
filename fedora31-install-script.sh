017b8b958bb3cb52b59adf0f0f5f7634b0d6005d
Stiffer <stiffer17@gmail.com>
2019-12-29 20:34:12 -0300

    agregado mas programas

--- fedora31-install-script.sh
+++ fedora31-install-script.sh
@@ -4,6 +4,8 @@
 # Force update the whole system to the latest and greatest
 ###
 
+#Before Install -> chmod u+x install.sh
+
 # -------------VARIABLES-----------#
 DIRECTORIO_DOWNLOADS="$HOME/Downloads/programas"
 
@@ -13,7 +15,7 @@ URL_DBEAVER="https://dbeaver.io/files/dbeaver-ce-latest-stable.x86_64.rpm"
 
 
 echo "Iniciando upgrade"
-
+sudo dnf update
 sudo dnf upgrade --best --allowerasing --refresh -y
 
 
@@ -22,13 +24,17 @@ sudo dnf upgrade --best --allowerasing --refresh -y
 sudo rpm --import https://packagecloud.io/AtomEditor/atom/gpgkey
 sudo sh -c 'echo -e "[Atom]\nname=Atom Editor\nbaseurl=https://packagecloud.io/AtomEditor/atom/el/7/\$basearch\nenabled=1\ngpgcheck=0\nrepo_gpgcheck=1\ngpgkey=https://packagecloud.io/AtomEditor/atom/gpgkey" > /etc/yum.repos.d/atom.repo'
 
-echo "upgrade instalando programas"
+sudo dnf update
 
+echo "upgrade instalando programas"
+sudo dnf copr enable ztefn/haguichi #para el haguichi
 sudo dnf install \
 -y \
 gimp \
 atom \
 snapd \
+haguichi \
+ffmpeg #Adds Codec Support to Firefox
 
 echo "PAquetes SNAP"
 sudo snap install telegram-desktop
